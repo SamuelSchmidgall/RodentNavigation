e063baabd6101d46b765fa67ced2b69e173b87da
domhuh <dhuh137@gmail.com>
2020-03-24 08:32:53 -0400

    added cheetah results

---
+++ crippled_half_cheetah_results/gated/view_rewards.py
@@ -0,0 +1,16 @@
+import pickle
+import random
+import matplotlib.pyplot as plt
+
+for i in range(1,4):
+	with open(f"./run_{i}/save_reward_gated_CrippledHalfCheetahBulletEnv_{i-1}.pkl",'rb') as f:
+		m = pickle.load(f)
+
+	rewards = []
+	for j in range(len(m)):
+		rewards.append(m[j][0])
+
+	plt.plot(rewards, label=f"run {i}")
+
+plt.legend()
+plt.show()
